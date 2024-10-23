1ec7fe0e4cff02f6dbe83f59c518e2e316db39b3
Matheus R Linhaus <matheuslinhauss@hotmial.com>
2024-10-23 13:02:38 -0300

    adicionando o processo de busca por departamento

--- ExercicioPadraoDAO/src/application/App.java
+++ ExercicioPadraoDAO/src/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
@@ -13,6 +16,13 @@ public class App {
 		System.out.println("Seller by Id");
 		Seller seller = sellerDao.findById(8);
 		System.out.println(seller);
+		
+		System.out.println("Seller by Department");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for (Seller sl : list) {
+			System.out.println(sl);
+		}
 
 	}
 
